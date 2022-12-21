0b349b80b694759075b54447b454bfd072bd598c
Kamil Bozek <kamil.bozek@gmail.com>
2022-12-21 17:40:06 +0100

    bongo cat

--- keyboards/splitkb/kyria/keymaps/kamilbozek/rules.mk
+++ keyboards/splitkb/kyria/keymaps/kamilbozek/rules.mk
@@ -2,7 +2,7 @@
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 
 # From Miryoku
 MOUSEKEY_ENABLE = no
@@ -11,3 +11,5 @@ AUTO_SHIFT_ENABLE = no
 TAP_DANCE_ENABLE = no
 CAPS_WORD_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
+
+WPM_ENABLE = yes
