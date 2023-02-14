5704bfeff990ba3d2dcbe1cd81ffd47744036f84
Kamil Bozek <kamil.bozek@gmail.com>
2023-02-14 11:08:12 +0100

    change layout

--- keyboards/handwired/macropad9x9/keymaps/default/keymap.c
+++ keyboards/handwired/macropad9x9/keymaps/default/keymap.c
@@ -11,8 +11,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┘
      */
     [0] = LAYOUT_ortho_3x3(
-        KC_P7,   KC_P8,   KC_P9,
-        KC_P4,   KC_P5,   KC_P6,
-        KC_P1,   KC_P2,   KC_P3
+        LGUI(KC_TAB),   LCTL(KC_UP),   LGUI(KC_TAB),
+        LCTL(KC_LEFT),   LCTL(KC_DOWN),   LCTL(KC_RIGHT),
+        LCA(KC_LEFT),   LCTL(LGUI(KC_F)),   LCA(KC_RIGHT)
     )
 };
