9bcac222e62cb4b8c1d25c8465b71368a049f725
Kamil Bozek <kamil.bozek@gmail.com>
2023-03-06 20:46:33 +0100

    improved custom symbol layer

--- keyboards/splitkb/kyria/keymaps/kamilbozek/keymap.c
+++ keyboards/splitkb/kyria/keymaps/kamilbozek/keymap.c
@@ -35,19 +35,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 
-// symbol layer ideas
-// ` < > " .  & x [ ] %
-// ! - + = #  | : ( ) ?
-// ^ / * \ .  ~ $ { } @
-
-
-// - _ : = () [] {} <> ? ,(base) .(base)
-// ->
-// =>
+// ` < > " & $ @ [ ] %
+// ! - + = # | : ( ) ?
+// ~ / * \ . x x { } ^
+//       _ -
     [_SYM] = LAYOUT_kamilbozek(
-    KC_GRV,            KC_LABK,           KC_RABK,           KC_DQUO,           KC_AMPR,           U_NA,              U_NA,              KC_LBRC,           KC_RBRC,           KC_PERC,     \
+    KC_GRV,            KC_LABK,           KC_RABK,           KC_DQUO,           KC_AMPR,           KC_DLR,            KC_AT,            KC_LBRC,           KC_RBRC,           KC_PERC,     \
     KC_EXLM,           KC_MINS,           KC_PLUS,           KC_EQL,            KC_HASH,           KC_PIPE,           KC_COLN,           KC_LPRN,           KC_RPRN,           KC_QUES,           \
-    KC_TILD,           KC_SLASH,          KC_ASTR,           KC_BSLS,           KC_DOT,            KC_CIRC,           KC_DLR,            KC_LCBR,           KC_RCBR,           KC_AT,              \
+    KC_TILD,           KC_SLASH,          KC_ASTR,           KC_BSLS,           KC_DOT,            U_NA,              U_NA,              KC_LCBR,           KC_RCBR,           KC_CIRC,              \
                                                              KC_UNDS,           KC_MINS,           U_NA,              U_NA
     ),
 
