084ea1670fea92d29803c13dbbd9b9a55dff0129
Kamil Bozek <kamil.bozek@gmail.com>
2024-01-07 20:03:08 +0100

    kamilbozek/slowpoke: default keymap

--- keyboards/kamilbozek/slowpoke/keymaps/default/keymap.c
+++ keyboards/kamilbozek/slowpoke/keymaps/default/keymap.c
@@ -17,5 +17,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_QUOT,
         KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,
                                             KC_TAB,     KC_SPC,     KC_ESC,     KC_BSPC
+    ),
+
+    [_NUM] = LAYOUT_split_3x5_2(
+        KC_SLSH,    KC_7,       KC_8,       KC_9,       KC_ASTR,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+        KC_MINS,    KC_4,       KC_5,       KC_6,       KC_PLUS,    XXXXXXX,    KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,
+        KC_PERC,    KC_1,       KC_2,       KC_3,       KC_PERC,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_ALGR,    XXXXXXX,
+                                            KC_0,       KC_DOT,     XXXXXXX,    XXXXXXX
+    ),
+
+// ` < > " & @ [ ] $ %
+// : - + = ; | ( ) ! ?
+// ~ / * # \ x { } . ^
+//       _ -
+    [_SYM] = LAYOUT_split_3x5_2(
+        KC_GRV,     KC_LABK,    KC_RABK,    KC_DQUO,    KC_AMPR,    KC_AT,      KC_LBRC,    KC_RBRC,    KC_DLR,     KC_PERC,
+        KC_COLN,    KC_MINS,    KC_PLUS,    KC_EQL,     KC_SCLN,    KC_PIPE,    KC_LPRN,    KC_RPRN,    KC_EXLM,    KC_QUES,
+        KC_TILD,    KC_SLASH,   KC_ASTR,    KC_HASH,    KC_BSLS,    XXXXXXX,    KC_LCBR,    KC_RCBR,    KC_DOT,     KC_CIRC,
+                                            KC_UNDS,    KC_MINS,    XXXXXXX,    XXXXXXX
+    ),
+
+    [_NAV] = LAYOUT_split_3x5_2(
+        XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    U_RDO,      U_PST,      U_CPY,      U_CUT,      U_UND,
+        KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    CW_TOGG,
+        XXXXXXX,    KC_ALGR,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     KC_ENT,
+                                            XXXXXXX,    XXXXXXX,    KC_ENT,     KC_DEL
+    ),
+
+    [_MEDIA] = LAYOUT_split_3x5_2(
+        XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+        KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX,    XXXXXXX,    KC_VOLD,    KC_VOLU,    XXXXXXX,    XXXXXXX,
+        XXXXXXX,    KC_ALGR,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+                                            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX
     )
 };
