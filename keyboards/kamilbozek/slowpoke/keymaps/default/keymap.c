e8504adc268d39fe764f3212d6eeb471f74093c2
Kamil Bozek <kamil.bozek@gmail.com>
2024-01-10 11:46:15 +0100

    kamilbozek/slowpoke: switching layers in keymap

--- keyboards/kamilbozek/slowpoke/keymaps/default/keymap.c
+++ keyboards/kamilbozek/slowpoke/keymaps/default/keymap.c
@@ -14,9 +14,9 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x5_2(
         KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,
-        KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_QUOT,
+        LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,KC_H, LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),
         KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,
-                                            KC_TAB,     KC_SPC,     KC_ESC,     KC_BSPC
+                                            LT(_MEDIA,KC_TAB), LT(_NAV,KC_SPC),   LT(_SYM,KC_ESC),   LT(_NUM,KC_BSPC)
     ),
 
     [_NUM] = LAYOUT_split_3x5_2(
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NAV] = LAYOUT_split_3x5_2(
         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    U_RDO,      U_PST,      U_CPY,      U_CUT,      U_UND,
-        KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    CW_TOGG,
+        KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    KC_ENT,
         XXXXXXX,    KC_ALGR,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     KC_ENT,
-                                            XXXXXXX,    XXXXXXX,    KC_ENT,     KC_DEL
+                                            XXXXXXX,    XXXXXXX,    CW_TOGG,     KC_DEL
     ),
 
     [_MEDIA] = LAYOUT_split_3x5_2(
