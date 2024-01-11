340dd1276fe59c7a00937fc4b713909977498fa5
Kamil Bozek <kamil.bozek@gmail.com>
2024-01-11 22:55:57 +0100

    kamilbozek/slowpoke: formatted code

--- keyboards/kamilbozek/slowpoke/keymaps/default/keymap.c
+++ keyboards/kamilbozek/slowpoke/keymaps/default/keymap.c
@@ -13,10 +13,10 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x5_2(
-        KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,
-        LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,KC_H, LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),
-        KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,
-                                            LT(_MEDIA,KC_TAB), LT(_NAV,KC_SPC),   LT(_SYM,KC_ESC),   LT(_NUM,KC_BSPC)
+        KC_Q,           KC_W,           KC_E,           KC_R,               KC_T,               KC_Y,               KC_U,               KC_I,           KC_O,           KC_P,
+        LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),       KC_G,               KC_H,               LSFT_T(KC_J),       LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_QUOT),
+        KC_Z,           KC_X,           KC_C,           KC_V,               KC_B,               KC_N,               KC_M,               KC_COMM,        KC_DOT,         KC_SLSH,
+                                                        LT(_MEDIA,KC_TAB),  LT(_NAV,KC_SPC),    LT(_SYM,KC_ESC),    LT(_NUM,KC_BSPC)
     ),
 
     [_NUM] = LAYOUT_split_3x5_2(
@@ -26,10 +26,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_0,       KC_DOT,     XXXXXXX,    XXXXXXX
     ),
 
-// ` < > " & @ [ ] $ %
-// : - + = ; | ( ) ! ?
-// ~ / * # \ x { } . ^
-//       _ -
     [_SYM] = LAYOUT_split_3x5_2(
         KC_GRV,     KC_LABK,    KC_RABK,    KC_DQUO,    KC_AMPR,    KC_AT,      KC_LBRC,    KC_RBRC,    KC_DLR,     KC_PERC,
         KC_COLN,    KC_MINS,    KC_PLUS,    KC_EQL,     KC_SCLN,    KC_PIPE,    KC_LPRN,    KC_RPRN,    KC_EXLM,    KC_QUES,
